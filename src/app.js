c3f47c9919a78777498291f925da5eadb5d667b3
Phillip Cantu <thereisphil@gmail.com>
2025-08-08 21:33:32 +0000

    Finished project. Had mentoring session with Gabriel.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
 
+  const whoTag = document.getElementById("who");
+  const actionTag = document.getElementById("action");
+  const whatTag = document.getElementById("what");
+  const whenTag = document.getElementById("when");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  function updateTag(tag, array) {
+    let i = Math.floor(Math.random() * array.length);
+    tag.innerHTML = array[i];
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  updateTag(whoTag, who);
+  updateTag(actionTag, action);
+  updateTag(whatTag, what);
+  updateTag(whenTag, when);
+
+  document.getElementById("generateBtn").addEventListener("click", () => {
+    updateTag(whoTag, who);
+    updateTag(actionTag, action);
+    updateTag(whatTag, what);
+    updateTag(whenTag, when);
+  });
 };
